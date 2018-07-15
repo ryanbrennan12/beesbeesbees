ab304b6c97445f1a4942ced056f8e5b6bc059b8f
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-07-14 22:00:57 -0400

    HoneyMakerBee complete

--- src/Bee.js
+++ src/Bee.js
@@ -2,5 +2,7 @@ var Bee = function() {
 	this.age = 5;
 	this.color = 'yellow';
 	this.food = new Grub().food;
+	this.job = 'keep on growing';
 };
 
+Bee.prototype.eat = new Grub().eat;
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
 var HoneyMakerBee = function() {
+	this.age = 10;
+	this.job = 'make honey';
+	this.color = new Bee().color;
+	this.food = new Grub().food;
+	this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype.eat = new Grub().eat;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+	this.honeyPot += 1;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+	this.honeyPot -= 1;
+}
