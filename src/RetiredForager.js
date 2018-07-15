dda4cf1838cce1ce7a66a3dd8e0ea11a9983ae35
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-07-14 22:20:15 -0400

    BeesBeeBees complete

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,18 @@
 var RetiredForagerBee = function() {
+	this.age = 40;
+	this.job = 'gamble';
+	this.canFly = false;
+	this.color = 'grey';
+	this.food =  new Grub().food;
+	this.treasureChest = new ForagerBee().treasureChest;
 };
 
+RetiredForagerBee.prototype.forage = function() {
+	return 'I am too old, let me play cards instead';
+}
+
+RetiredForagerBee.prototype.eat = new Grub().eat;
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+	this.treasureChest.push(treasure);
+}
