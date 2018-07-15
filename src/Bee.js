8a3e8c5fa99ff303a5228543c8c2725d78b0acee
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-07-14 22:37:57 -0400

    Corrected to use .constructor

--- src/Bee.js
+++ src/Bee.js
@@ -1,8 +1,10 @@
 var Bee = function() {
+	Grub.call(this);
+
 	this.age = 5;
 	this.color = 'yellow';
-	this.food = new Grub().food;
 	this.job = 'keep on growing';
 };
 
-Bee.prototype.eat = new Grub().eat;
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
