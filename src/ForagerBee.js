e5f49c24f8f7c8d92f0440936774cbcceaa4cad3
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-07-14 22:11:34 -0400

    ForagerBee complete

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
 var ForagerBee = function() {
+	this.age = 10;
+	this.job = 'find pollen';
+	this.color = new Bee().color;
+	this.food = new Grub().food;
+	this.canFly = true;
+	this.treasureChest = [];
 };
 
+ForagerBee.prototype.eat = new Grub().eat;
+
+ForagerBee.prototype.forage = function(treasure) {
+	this.treasureChest.push(treasure)
+}
