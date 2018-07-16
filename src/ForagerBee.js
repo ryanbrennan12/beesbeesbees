bd795139a9f28c2ef495064eb164280b511a44c0
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-07-15 22:05:01 -0400

    ForagerBee fixed per Learn Requirements

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,14 +1,18 @@
 var ForagerBee = function() {
+	
+	HoneyMakerBee.call(this);
+	
 	this.age = 10;
 	this.job = 'find pollen';
-	this.color = new Bee().color;
-	this.food = new Grub().food;
 	this.canFly = true;
 	this.treasureChest = [];
 };
 
-ForagerBee.prototype.eat = new Grub().eat;
+ForagerBee.prototype = Object.create(HoneyMakerBee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
 
 ForagerBee.prototype.forage = function(treasure) {
-	this.treasureChest.push(treasure)
+	this.treasureChest.push(treasure);
 }
+
+
