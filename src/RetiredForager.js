6b07be9a3381af5de05297e10128186ad6462710
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-07-15 22:12:37 -0400

    RetiredForagerBee fixed per Learn Requirements

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,18 +1,19 @@
 var RetiredForagerBee = function() {
+	ForagerBee.call(this);
+
 	this.age = 40;
 	this.job = 'gamble';
 	this.canFly = false;
 	this.color = 'grey';
-	this.food =  new Grub().food;
-	this.treasureChest = new ForagerBee().treasureChest;
 };
 
-RetiredForagerBee.prototype.forage = function() {
-	return 'I am too old, let me play cards instead';
-}
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
 
-RetiredForagerBee.prototype.eat = new Grub().eat;
+RetiredForagerBee.prototype.forage = function() {
+	return 'I am too old, let me play cards instead'
+}
 
 RetiredForagerBee.prototype.gamble = function(treasure) {
 	this.treasureChest.push(treasure);
 }
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
